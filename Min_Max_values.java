e619b901a2115f95e2785cafcb429ff795eaa19d
LakshmishreeR26 <139939070+LakshmishreeR26@users.noreply.github.com>
2025-06-04 09:05:40 +0530

    Create Min_Max_values.java

---
+++ Min_Max_values.java
@@ -0,0 +1,8 @@
+public class Max_Min_values {
+    public static void main(String[] args) {
+        System.out.println("Max Integer Value: "+Integer.MAX_VALUE);
+        System.out.println("Min Integer Value: "+Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE + 1);
+        System.out.println(Integer.MIN_VALUE - 1);
+    }
+}
