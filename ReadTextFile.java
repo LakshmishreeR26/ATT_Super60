741c2d6d607ef81eeb311b3d9b7669278d26afb5
LakshmishreeR26 <139939070+LakshmishreeR26@users.noreply.github.com>
2025-06-04 12:59:17 +0530

    Create ReadTextFile.java

---
+++ ReadTextFile.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class ReadTextFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File ("./Java/DSA/bnmit.txt");
+        Scanner reader = new Scanner(file);
+        while (reader.hasNext()) {
+            String data =reader.nextLine();
+            System.out.println(data);
+        }
+        reader.close();
+    }
+}
