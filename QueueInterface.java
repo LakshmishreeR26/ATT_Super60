5433203a060cd7d4a0d7248932f8291b1937e8c7
LakshmishreeR26 <139939070+LakshmishreeR26@users.noreply.github.com>
2025-06-05 15:22:21 +0530

    Create QueueInterface.java

---
+++ QueueInterface.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+interface ABC{
+    void display();
+}
+class Demo implements ABC {
+    @Override
+    public void display() {
+        System.out.println("Hi....!!!");
+    }
+}
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        queue.offer(13);
+        queue.offer(12);
+        queue.offer(16);
+        System.out.println("Queue:"+queue);
+        queue.poll();
+        System.out.println("Queue:"+queue);
+        System.out.println("Front:" +queue.peek());
+    }
+
+}
