e361a06928a45e0f0dc2303cc22ab4a3f81ee383
LakshmishreeR26 <139939070+LakshmishreeR26@users.noreply.github.com>
2025-06-05 12:23:05 +0530

    Update Leetcode_1823.java

--- Leetcode_1823.java
+++ Leetcode_1823.java
@@ -14,3 +14,23 @@ public class Solution_1823_LC {
         System.out.println(solution.findWinner(5,2));
     }
 }
+ Optimised Code:
+     public class Solution_1823_LC {
+    int solve(int n, int k){
+        if(n==1){
+            return 0;
+        }
+        return (solve(n-1,k)+k)%n;
+    }
+    int findWinner(int n, int k){
+        int winner = 0;
+        for(int i=2;i<=n;i++){
+            winner=(winner+k)%i;
+        }
+        return winner+1;
+    }
+    public static void main(String[] args) {
+        Solution_1823_LC solution = new Solution_1823_LC();
+        System.out.println(solution.findWinner(5,2));
+    }
+}
