7e8e2109fe9abbd4d2532c2734eb472f8247dd5b
LakshmishreeR26 <139939070+LakshmishreeR26@users.noreply.github.com>
2025-06-14 15:22:44 +0530

    Create FibonacciSpace.java

---
+++ FibonacciSpace.java
@@ -0,0 +1,18 @@
+public class FibonacciSpace {
+    public static int fib(int n){
+        if(n<=1)
+            return n;
+        int p2=0;
+        int p1=1;
+        for(int i=2;i<=n;i++){
+            int curr=p2+p1;
+            p2=p1;
+            p1=curr;
+        }
+        return p1;
+    }
+    public static void main(String[] args) {
+        int n=6;
+        System.out.println("The fibonacci of " + n + " th is " + fib(n));
+    }
+}
